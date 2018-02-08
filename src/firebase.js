667981b646940a0de80a2e5061550ec3195b1335
Luthfir <luthfirrahman04@gmail.com>
2018-02-08 11:51:03 +0700

    update for auto deploy heroku

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 
 const config = {
-    apiKey: "YourApiKey",
-    authDomain: "YourAuthDomain",
-    databaseURL: "YourDatabaseURL",
-    projectId: "YourProjectId",
-    storageBucket: "YourStorageBucketId",
-    messagingSenderId: "YourMessagingSenderId"
+    apiKey: "AIzaSyB27sTAlar6ISEOcid4RHf_mf3N9sJNk0k",
+    authDomain: "anochat-2be0c.firebaseapp.com",
+    databaseURL: "https://anochat-2be0c.firebaseio.com",
+    projectId: "anochat-2be0c",
+    storageBucket: "anochat-2be0c.appspot.com",
+    messagingSenderId: "216727983013"
 }
 
 export const firebaseApp = firebase.initializeApp(config);
